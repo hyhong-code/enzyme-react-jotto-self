908c71b68974ff54914eb43fbdfbdafe381f533e
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-07-01 20:37:51 -0700

    Rewrite Enzyme imports and test utils

--- test/testUtils.js
+++ test/testUtils.js
@@ -6,7 +6,6 @@ import checkPropTypes from "check-prop-types";
  * @param {string} val - value data-test attribute
  * @returns {ShallowWrapper}
  */
-
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
 };
